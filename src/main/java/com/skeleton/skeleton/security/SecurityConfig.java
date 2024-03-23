21ba201b565fa4e649473b6a9c962a4b760d1b54
Rickie Natividad <rickienatividad85@gmail.com>
2024-03-23 17:18:25 -0500

    beginning to refactor security

--- src/main/java/com/skeleton/skeleton/security/SecurityConfig.java
+++ src/main/java/com/skeleton/skeleton/security/SecurityConfig.java
@@ -2,16 +2,18 @@ package com.skeleton.skeleton.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.cors().disable()
-        .csrf().disable()
-        .authorizeHttpRequests().requestMatchers("/users").permitAll().and()
-        .build();
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeHttpRequests()
+        .requestMatchers("/users").permitAll();
+    return httpSecurity.build();
   }
 }
