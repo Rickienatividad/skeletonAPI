10ca040eff9ff91c57185bf63176d3c49e9c8fa7
Rickie Natividad <rickienatividad85@gmail.com>
2024-05-08 20:07:51 -0500

    adds index action for roles

--- src/main/java/com/skeleton/skeleton/controllers/RoleController.java
+++ src/main/java/com/skeleton/skeleton/controllers/RoleController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.skeleton.skeleton.requestObjects.CreateRoleObj;
 import com.skeleton.skeleton.services.RoleService;
+import com.skeleton.skeleton.models.Role;
 
+import java.util.List;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/roles")
@@ -29,4 +33,10 @@ public class RoleController {
   public ResponseEntity<?> createRoleEntity(@RequestBody CreateRoleObj createRoleObj) {
     return roleService.createRole(createRoleObj);
   }
+
+  @GetMapping("")
+  public List<Role> getRoles() {
+    return roleService.getRoles();
+  }
+
 }
